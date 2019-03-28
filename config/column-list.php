385ff53d2311bbd55d929e723f162dee33ffd510
Roman Zipp <ich@ich.wtf>
2019-03-28 14:04:52 +0100

    Add config

--- config/column-list.php
+++ config/column-list.php
@@ -1,3 +1,16 @@
 <?php
 
-return [];
+return [
+    'display_columns' => [
+        'name'           => true,
+        'laravel'        => true,
+        'type'           => true,
+        'length'         => true,
+        'nullable'       => true,
+        'auto_increment' => true,
+        'default'        => true,
+        'comment'        => true,
+    ],
+    'emojis'          => true,
+    'colors'          => true,
+];
